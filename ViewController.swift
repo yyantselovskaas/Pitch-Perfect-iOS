a1052291c91ba7bb18e02003e46133f49ddd9f61
Yana Yantselovska <yyantselovska@arrowstream.com>
2018-03-17 15:42:39 -0500

    3. ViewController and Multiple Views
    Created new view and navigation to it, added buttons enabling/disabling, edited buttons locations and added button icons

--- ViewController.swift
+++ ViewController.swift
@@ -11,10 +11,17 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("ViewWillAppear called")
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,12 +30,15 @@ class ViewController: UIViewController {
     }
     
     @IBAction func recordAudio(_ sender: Any) {
-        print("record button was pressed")
         recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
     }
     
     @IBAction func stopRecording(_ sender: Any) {
-        print("stop recording button was pressed")
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        recordingLabel.text = "Tap to Record"
     }
 }
 
